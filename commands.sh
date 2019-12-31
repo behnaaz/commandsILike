36c40a3be6829cc459107c46c8662e9b5c0471fb
BC <behnaaz@users.noreply.github.com>
2019-12-31 11:49:43 +0100

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -2,3 +2,10 @@ git ls-files --deleted | xargs git rm
 
 response=`curl -k -s -X GET --url "<url_of_the_request>"`
 echo "${response}"
+
+trap 'rm -f /tmp/xyz$$; exit' ERR EXIT
+trap -l
+
+watch free -m
+
+watch -n 10 script.sh
